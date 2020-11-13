28d3c46d8fc2fa19fe85f491c6109bbf60ca3206
Nakwon Yun <cornflake2161@gmail.com>
2020-11-14 05:36:41 +0900

    [맵 프래그먼트]  - 변동사항 없음
    [통계 프래그먼트]
     - 가독성을 위해 단계 텍스트 뷰 삭제(코드의 유동적인 작동에 의해 다시 추가할 수도 있음)
     - [국내-세계]버튼 테두리 디자인 xml 파일 추가
     - 스크롤 뷰 추가
    
    [그래프 프래그먼트]
     - 변동사항 없음

--- app/src/main/java/com/team12/coronawatch/MainActivity.java
+++ app/src/main/java/com/team12/coronawatch/MainActivity.java
@@ -1,7 +1,9 @@
 package com.team12.coronawatch;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,35 +12,44 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment selectedFragment;
+    private View decorView;
+    private int uiOption;
+
+    private void init() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nv);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        bottomNavigationView.setSelectedItemId(R.id.statFragment);
+        selectedFragment = new StatFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment,
+                selectedFragment).commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        init();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-                    switch (item.getItemId()) {
-                        case R.id.mapsFragment:
-                            selectedFragment = new MapsFragment();
-                            break;
-                        case R.id.statFragment:
-                            selectedFragment = new StatFragment();
-                            break;
-                        case R.id.graphFragment:
-                            selectedFragment = new GraphFragment();
-                            break;
+                    selectedFragment = null;
+                    int id = item.getItemId();
+                    if (id == R.id.mapsFragment) {
+                        selectedFragment = new MapsFragment();
+                    } else if (id == R.id.statFragment) {
+                        selectedFragment = new StatFragment();
+                    } else if (id == R.id.graphFragment) {
+                        selectedFragment = new GraphFragment();
                     }
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment,
-                            selectedFragment).commit();
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment,
+                                selectedFragment).commit();
+                    }
 
                     return true;
                 }
